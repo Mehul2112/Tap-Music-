5008e750a826885a9b52d5f4f2c9c551bd06951d
Mehul2112 <72200573+Mehul2112@users.noreply.github.com>
2021-01-13 15:31:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#217EB8",
+    "#41B8C1",
+    "#51D4AC",
+    "#FDD911",
+    "#c75a7e",
+    "rgb(255, 3, 3)"
+  ];
+
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      createBubble(index);
+    });
+  });
+
+  const createBubble = index => {
+    //Create bubbles
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = `jump 1s ease`;
+    bubble.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    });
+  };
+});
